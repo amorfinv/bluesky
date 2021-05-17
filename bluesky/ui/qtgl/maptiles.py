0fe7c613c1f5b2c7bb2f1b97869cac9e1dcac8cf
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2021-05-17 21:17:52 +0200

    simplify

--- bluesky/ui/qtgl/maptiles.py
+++ bluesky/ui/qtgl/maptiles.py
@@ -15,61 +15,56 @@ from .glhelpers import RenderObject
 bs.settings.set_variable_defaults(
     mpt_path='data/graphics', mpt_server='opentopomap', tile_standard='osm',
     enable_tiles=True,
-    mpt_url=['https://a.tile.opentopomap.org/{z}/{x}/{y}.png',
-             'https://b.tile.opentopomap.org/{z}/{x}/{y}.png',
-             'https://c.tile.opentopomap.org/{z}/{x}/{y}.png'])
+    mpt_url=['https://a.tile.opentopomap.org/{z}/{x}/{y}.png'])
+
 class MapTiles(Entity):
     """
     Default map server is from OpenTopoMap. As of March 12, 2021 data is open to use. https://opentopomap.org/about
 
     TODO List:
-        -Texture upload in multiple threads. Wait until new qt implementation
-        -Disappearing screen bug when panning maptiles
-        -vertex shader different projection
-        -relative zoom also based on image size
+        - Texture upload in multiple threads. Wait until new qt implementation
+        - Disappearing screen bug when panning maptiles
+        - Vertex shader different projection
+        - Relative zoom also based on image size
 
     Future ideas:
-        -Create license based on text
-        -make stack command better
-        -figure out how to use sources with multiple servers.
-        -fix issue with license
-        -accept other types of map tile formats. Like TMS or WMTS
+        - Create license text and add to image
+        - Make stack command 
+        - Figure out how to use sources with multiple servers.
+        - Accept other types of map tile formats. Like TMS or WMTS
         https://alastaira.wordpress.com/2011/07/06/converting-tms-tile-coordinates-to-googlebingosm-tile-coordinates/
 
-    tile_standards:
-        -'osm' standard: url contains tile path "{z}/{x}/{y}.png"
-         Example of sources that use osm standard:
-            -OpenTopoMap: https://opentopomap.org/
-                -This is the default map tiler for BlueSky. As of March 15, 2021 the tiles are open for download and use
-                 as long as credit is given to OpenTopoMap. Please refer to https://opentopomap.org/about.
-                 OpenTopoMap tries to update tiles every 4 weeks.
-            -OpenStreetMap: https://www.openstreetmap.org/
-                -At the moment OpenStreetMap can only be accessed with a Valid User-Agent. Please contact OSM
-                 to get a valid HTTP User-Agent and alter the download_tile() method so that a valid User-Agent
-                can be passed. Refer to tile usage policy: https://operations.osmfoundation.org/policies/tiles/
-            -OpenStreetMap Germany: https://www.openstreetmap.de/
-                -Unlike the regular OpenStreetMap, OSM germany does not require a valid HTTP User-Agent. Please refer
-                 to the tile usage policy: https://www.openstreetmap.de/germanstyle.html.
-            -maptiler: https://www.maptiler.com/
-                -Maptiler is a commercial product. Please refer to https://www.maptiler.com/cloud/terms/ for terms of
-                 service.
-            -local_host:
-                -Create your own tiles and serve them locally. Learn how to generate tiles at https://openmaptiles.org/.
-                 Use tileserver-GL https://tileserver.readthedocs.io/en/latest/index.html to render and serve the tiles.
-                 Url example is: http://localhost:8080/styles/basic-preview/{z}/{x}/{y}.png
+    'osm' tile standard: url contains tile path "{z}/{x}/{y}.png". Example of sources that use osm standard:
+        - OpenTopoMap: https://opentopomap.org/
+            - This is the default map tiler for BlueSky. As of March 15, 2021 the tiles are open for download and use
+              as long as credit is given to OpenTopoMap. Please refer to https://opentopomap.org/about.
+              OpenTopoMap tries to update tiles every 4 weeks.
+        - OpenStreetMap: https://www.openstreetmap.org/
+            - At the moment OpenStreetMap can only be accessed with a Valid User-Agent. Please contact OSM
+              to get a valid HTTP User-Agent and alter the download_tile() method so that a valid User-Agent
+              can be passed. Refer to tile usage policy: https://operations.osmfoundation.org/policies/tiles/
+        - OpenStreetMap Germany: https://www.openstreetmap.de/
+            - Unlike the regular OpenStreetMap, OSM germany does not require a valid HTTP User-Agent. Please refer
+              to the tile usage policy: https://www.openstreetmap.de/germanstyle.html.
+        - maptiler: https://www.maptiler.com/
+            - Maptiler is a commercial product. Please refer to https://www.maptiler.com/cloud/terms/ for terms of
+              service.
+        - local_host:
+            - Create your own tiles and serve them locally. Learn how to generate tiles at https://openmaptiles.org/.
+               Use tileserver-GL https://tileserver.readthedocs.io/en/latest/index.html to render and serve the tiles.
+               Url example is: http://localhost:8080/styles/basic-preview/{z}/{x}/{y}.png
+
+    Other resources
+        - information on tile storage info based on bbox.
+          https://tools.geofabrik.de/calc/#type=geofabrik_standard&bbox=-80.448849,25.625192,-80.104825,25.90675
+
     """
 
     def __init__(self, radar_widget):
         """
-        :param lat1: FLOAT, Latitude 1 of bounding box (north)
-        :param lon1: FLOAT, Longitude 1 of bounding box (west)
-        :param lat2: FLOAT, Latitude 2 of bounding box (south)
-        :param lon2: FLOAT, Longitude 2 of bounding box (east)
-        :param zoom_level: INTEGER, Zoom level for map tiles. 14 or 15 is recommended to limit number of requests
-                           see the link below for size estimation of bbox:
-            https://tools.geofabrik.de/calc/#type=geofabrik_standard&bbox=-80.448849,25.625192,-80.104825,25.90675
+        :param radar_widget: bring in radar widget to get shaders and screen information.
         """
-
+        # Inherit from BlueSky Entity
         super().__init__()
 
         # radar widget used to bring in shaders and screen size
@@ -79,7 +74,7 @@ class MapTiles(Entity):
         self.enable_tiles = bs.settings.enable_tiles
 
         # create tile directory path
-        self.tile_dir = path.join(bs.settings.mpt_path,'tiles', bs.settings.mpt_server)
+        self.tile_dir = path.join(bs.settings.mpt_path, bs.settings.mpt_server)
 
         # check for errors in config file url and set url_prefix and url_suffix for downloading of tiles
         img_std = '{z}/{x}/{y}'
@@ -91,7 +86,7 @@ class MapTiles(Entity):
             if 'png' in self.url_suffix:
                 self.tile_format = '.png'
             else:
-                print('Only accepting png formats at the moment')
+                print('Only accepting PNG formats at the moment')
                 print('Failed to load map tiles!!')
                 self.enable_tiles = False
         except ValueError:
@@ -102,6 +97,7 @@ class MapTiles(Entity):
         
 
         # Bounding box coordinates and zoom level initialization
+        # lat1 (north), lon1(west), lat2(south), lon2(east), zoom_level (map tile zoom level)
         self.lat1 = None
         self.lon1 = None
         self.lat2 = None
@@ -122,7 +118,8 @@ class MapTiles(Entity):
         self.tile_size = 0
         self.bbox_corners = None
 
-    # Drawing functions start here
+    ''' Drawing functions start here '''
+
     def tile_reload(self):
         # Tile reloading from screen bbox. Note that tiles are really only good for a zoom level greater than 8.
         # Anything smaller appears very deformed due to difference in projections of bluesky and maptiles.
@@ -133,7 +130,7 @@ class MapTiles(Entity):
         # clear everything to start fresh
         self.clear_tiles()
 
-        # Get screen bbox coordinates
+        # Get screen bbox coordinates for tile downloading
         self.lat1, self.lon1 = self.radar_widget.pixelCoordsToLatLon(0, 0)
         self.lat2, self.lon2 = self.radar_widget.pixelCoordsToLatLon(self.radar_widget.width, self.radar_widget.height)
 
@@ -173,33 +170,38 @@ class MapTiles(Entity):
         self.tile_load()
 
     def tile_load(self):
-        # create a tile array
+        # create a tile array and texture offset values
         self.create_tile_array()
 
         # process tiles, download tiles if necessary
         self.process_tiles()
 
-        # Bind tiles as textures. TODO: figure out faster way to send img_data information (parallel)
+        # Bind texture aray and give image data. 
         
-        # Number of pixels in tile. Open one image and check
+        # Calculate pixel size in a single tile by opening one image and checking. (square tiles)
         self.tile_size = Image.open(self.local_paths[0]).height
 
-        # number of tiles in texture
+        # Calculate pixel size needed for the texture array
         self.tex_width = self.tile_size * self.tex_columns
         self.tex_height = self.tile_size * self.tex_rows
 
-        # create texture
+        # Create texture and bind.
+        # For some reason code only works when adding texture name to a list
         texture = gl.glGenTextures(1)
         self.map_textures.append(texture)
         gl.glBindTexture(gl.GL_TEXTURE_2D_ARRAY, self.map_textures[0])
+
+        # Specify storage needed for the texture array
         gl.glTexStorage3D(gl.GL_TEXTURE_2D_ARRAY, 1, gl.GL_RGB8, self.tex_width, self.tex_height, 1)
 
-        # send image data to texture array.
+        # Loop to send image data to texture array.
+        # TODO: figure out faster way to send img_data byte information (perhaps in multiple threads) or maybe QT does this?
         for item in self.local_paths_offset:
             image_path = item[0]
             offset_y = item[1][0] * self.tile_size
             offset_x = item[1][1] * self.tile_size
 
+            # open image, get byte information and specify subimage of texture array
             image = Image.open(image_path)
             img_data = image.tobytes()
             gl.glTexSubImage3D(gl.GL_TEXTURE_2D_ARRAY, 0, offset_x, offset_y, 0, self.tile_size, self.tile_size, 1,
@@ -212,7 +214,7 @@ class MapTiles(Entity):
 
     def tile_render(self):
 
-        # Vertex array object for the 2D texture array
+        # Create vertex array object for the 2D texture array
         texvertices = np.array(self.bbox_corners, dtype=np.float32)
         textexcoords = np.array([(1, 1), (1, 0), (0, 0), (0, 1)], dtype=np.float32)
         self.tiles.append(RenderObject(gl.GL_TRIANGLE_FAN, vertex=texvertices, texcoords=textexcoords))
@@ -226,37 +228,38 @@ class MapTiles(Entity):
         self.tiles[0].draw()
 
     def clear_tiles(self):
-        # unbind and delete textures
+        # unbind and delete textures from memory
         for i in range(len(self.tiles)):
             self.tiles[i].unbind_all()
 
         gl.glDeleteTextures(len(self.map_textures), self.map_textures)
 
-        # clear variables for new bounding box
+        # Clear variables for new tiles
         self.local_paths = []
         self.tiles = []
         self.map_textures = []
         self.local_paths_offset = []
         self.tile_offset = []
 
-    # Non-drawing functions from here on
+    ''' Non-drawing functions start here '''
+
     def process_tiles(self):
-        # Download tiles in multiple threads. If download fails self.enable_tiles = False
+        # Download tiles in multiple threads
         with concurrent.futures.ThreadPoolExecutor() as executor:
             executor.map(self.download_tile, self.tile_array)
 
-        # Create local path names to access
+        # Create local path names so texture array can access them
         for item in self.tile_array:
+            # create image paths
             x = item[0]  # tile x
             y = item[1]  # tile y
-
             img_path = path.join(str(self.zoom_level), str(x), str(y))
             local_path = path.join(self.tile_dir, img_path + self.tile_format)
 
-            # create array of local paths for later use
+            # create array of local paths for texture array
             self.local_paths.append(local_path)
 
-        # Create local path dictionary with image offsets
+        # Create a list with tuples of local paths and their corresponding texture array offset
         self.local_paths_offset = list(zip(self.local_paths, self.tile_offset))
 
     def create_tile_array(self):
@@ -264,18 +267,22 @@ class MapTiles(Entity):
         tile_nw, tile_ne, tile_sw, tile_se = self.bbox_corner_tiles(self.lat1, self.lon1,
                                                                     self.lat2, self.lon2, self.zoom_level)
 
-        # Get bbox corner lat/lon for rendering
+        # Get bbox corner lat/lon for rendering (texvertices). This is usually a larger bounding box than screen
         self.bbox_corners = self.bbox_latlon(tile_nw, tile_ne, tile_sw, tile_se, self.zoom_level)
 
-        # Find size of 2D array for tiles
+        # Find size of a 2D array for tiles and create the array.
+        # tex_columns an tex_rows are the number of columns and rows, respectively, in 2D array.
+        # This 2D array has the same size as the texture array that will be rendered by OpenGL.
+        # tile_array contains a (x,y) tuple with tile numbers (slippy-map tile number)
         self.tex_columns = tile_ne[0] - tile_nw[0] + 1
         self.tex_rows = tile_sw[1] - tile_nw[1] + 1
         self.tile_array = np.zeros((self.tex_rows, self.tex_columns), [('x_loc', int), ('y_loc', int)])
 
-        # fill NW corner of tile array
+        # fill NW corner of tile array to give a starting point for 2D array
         self.tile_array[0, 0] = tile_nw
 
-        # loop through rows. x is constant in one row
+        # Loop through each element of the array and fill with corresponding tile number
+        # loop through rows. x is constant in the same row
         for row in range(self.tex_rows):
             # for each row loop through a column. y is constant in the same column.
             for col in range(self.tex_columns):
@@ -291,66 +298,62 @@ class MapTiles(Entity):
                     self.tile_array[row, col] = (self.tile_array[row, col - 1][0] + 1,
                                                  self.tile_array[row, col - 1][1])
 
-                # create tile offsets of texture array and create list of tuples with the local_path
+                # Create list of tile offsets of texture array. 
+                # These values tell you how much to offset (row, col) the tile from the top left corner of texture array
                 self.tile_offset.append((row, col))
 
         # flatten tile array
         self.tile_array = self.tile_array.flatten()
 
     def download_tile(self, tile):
-        # Only run loop while downloading did not fail. if download tiles fails self.enable_tiles is set to false
-        if self.enable_tiles:
+        # Create paths from tile zoom, x, y
+        # osm tile_format downloads have the same image path as local path. 
+        # Note that url_img_path is a string due to difference in path strings per operating system. "/" vs "\"
+        x = tile[0]  # tile x
+        y = tile[1]  # tile y
+        img_path = path.join(str(self.zoom_level), str(x), str(y))
+        url_img_path = f'{str(self.zoom_level)}/{str(x)}/{str(y)}'
+        local_path = path.join(self.tile_dir, img_path + self.tile_format)
 
-            # Create image paths, raw is unaltered image, local_path is one that is shown on screen
-            x = tile[0]  # tile x
-            y = tile[1]  # tile y
-            img_path = path.join(str(self.zoom_level), str(x), str(y))
-            raw_local_path = path.join(self.tile_dir, img_path + self.tile_format)
+        # Only go into loop if the full_local_path does not exist. If previously downloaded, it will ignore
+        if not path.exists(local_path):
 
-            # Download tile if it has not been downloaded
-            if not path.exists(raw_local_path):
+            # create new path, first create directories for zoom level and x
+            img_dirs = path.join(self.tile_dir, str(self.zoom_level), str(x))
 
-                # create new paths, first create directories for zoom level and x
-                img_dirs = path.join(self.tile_dir, str(self.zoom_level), str(x))
+            # Create directory only if it doesn't exist
+            try:
+                makedirs(img_dirs)
+            except FileExistsError:
+                pass
+            
+            # Create image url for downloading
+            image_url = self.url_prefix + url_img_path + self.url_suffix
 
-                # Create directory only if it doesn't exist
+            # request tile from server and save to full_local_path
+            with open(local_path, "wb") as infile:
                 try:
-                    makedirs(img_dirs)
-                except FileExistsError:
-                    pass
+                    url_request = urlopen(image_url)
+                    infile.write(url_request.read())
+                except URLError:
+                    print(f'Failed to download tiles. Ensure that url is valid: {image_url}')
+                    remove(local_path)
 
-                # download image from server
-              
-                # osm tile_format downloads have the same image path as local folder
-                url_img_path = f'{str(self.zoom_level)}/{str(x)}/{str(y)}'
+            # Convert to RGB for OpenGL and save
+            img = Image.open(local_path).convert('RGB')
+            img.save(local_path)
 
-                image_url = self.url_prefix + url_img_path + self.url_suffix
+    '''
+    ----------------------------------------------------------------------
+    Translates between lat/long and the slippy-map tile numbering scheme
+    
+    Code was adapted from:
+    http://wiki.openstreetmap.org/index.php/Slippy_map_tilenames
 
-                # request
-                with open(raw_local_path, "wb") as infile:
-                    try:
-                        url_request = urlopen(image_url)
-                        infile.write(url_request.read())
-                    except URLError:
-                        print(f'Failed to download tiles. Ensure that url is valid: {image_url}')
-                        remove(raw_local_path)
-
-                        # cancel map tile downloading loop
-                        self.enable_tiles = False
-
-                # Convert to RGBA for OpenGL
-                img = Image.open(raw_local_path).convert('RGB')
-                img.save(raw_local_path)
-
-    # ----------------------------------------------------------------------
-    # Translates between lat/long and the slippy-map tile numbering scheme
-    #
-    # Code was adapted from:
-    # http://wiki.openstreetmap.org/index.php/Slippy_map_tilenames
-    #
-    # Written by Oliver White, 2007
-    # This file is public-domain
-    # ----------------------------------------------------------------------
+    Written by Oliver White, 2007
+    This file is public-domain
+    ----------------------------------------------------------------------
+    '''
 
     def bbox_latlon(self, tile_nw, tile_ne, tile_sw, tile_se, z):
         # NW Tuple
