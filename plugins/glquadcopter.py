7db509f3a8555a02e5005214ce3cbf946551d886
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2021-11-10 12:58:07 +0100

    show quadcopter

--- plugins/glquadcopter.py
+++ plugins/glquadcopter.py
@@ -1,14 +1,8 @@
-""" Bird traffic gui plugin """
+""" PLugin to replace aircraft symbol with a quadcopter """
 import numpy as np
 from os import path
 
-import bluesky as bs
-from bluesky import ui 
-from bluesky.ui import palette
 from bluesky import settings
-import bluesky.ui.qtgl.glhelpers as glh
-from bluesky.tools.aero import ft
-from bluesky.ui.qtgl.guiclient import UPDATE_ALL
 from bluesky.ui.qtgl.gltraffic import Traffic
 
 # Register settings defaults
@@ -16,14 +10,7 @@ settings.set_variable_defaults(
     text_size=13, ac_size=16,
     asas_vmin=200.0, asas_vmax=500.0)
 
-# Static defines
-MAX_NAIRCRAFT = 10000
-MAX_NCONFLICTS = 25000
-MAX_ROUTE_LENGTH = 500
-ROUTE_SIZE = 500
-TRAILS_SIZE = 1000000
-
-### Initialization function of your plugin.
+### Initialization function of plugin.
 def init_plugin():
     config = {
         'plugin_name':     'QUADGUI',
