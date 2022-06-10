bc44d79b7d96f2c96f3c7aade3f06f53783be0ae
andubadea <37139260+andubadea@users.noreply.github.com>
2022-06-10 09:19:53 +0200

    Improved commenting for simplemetrics.

--- plugins/icratMetrics/simplemetrics.py
+++ plugins/icratMetrics/simplemetrics.py
@@ -31,11 +31,16 @@ class SimpleMetrics(Entity):
         self.conflict_no = 0
         self.los_no = 0
         
+        # By default, the plugin is disabled.
+        self.enabled = False
+        
         # Metrics that are aircraft specific can be handled in trafarrays
         with self.settrafarrays():
             self.distance_horizontal = np.array([])
             self.distance_vertical = np.array([])
             self.spawntime = np.array([])
+            
+        return
             
     # Whenever we use trafarrays, we need to define a create function to set the initial values
     # This function is always called when an aircraft is created
@@ -45,9 +50,15 @@ class SimpleMetrics(Entity):
         self.distance_vertical[-n:] = 0
         self.spawntime[-n:] = sim.simt
         
+        return
+        
     # Just as the create function, the delete function is called when an aircraft is deleted
     # In this case, we want to log the data when an aircraft is deleted
     def delete(self, acidxs):
+        # Only run the function the plugin is set as enabled
+        if not self.enabled:
+            return
+        
         # We print stuff here, but we can also save all this data to a file. 
         # The delete function can be called for several aircraft, so we need to
         # for loop through the received aircraft indexes.
@@ -57,11 +68,17 @@ class SimpleMetrics(Entity):
             print(f'Aircraft {traf.id[acidx]} was deleted at {sim.simt}.')
             print(f'Aircraft {traf.id[acidx]} flew {self.distance_horizontal[acidx]} m horizontally.')
             print(f'Aircraft {traf.id[acidx]} flew {self.distance_vertical[acidx]} m vertically.')
+            
+        return
         
     
     # The update function is a timed function, meaning it is run once every "dt" seconds.
     @timed_function(dt = 1)
     def update_metrics(self):
+        # Only run the function the plugin is set as enabled
+        if not self.enabled:
+            return
+        
         # First we do the conflicts. We only want unique conflicts, so we check the traffic
         # object for new conflicts that have appeared since the last update.
         confpairs_new = list(set(traf.cd.confpairs) - self.prevconfpairs)
@@ -81,5 +98,16 @@ class SimpleMetrics(Entity):
         self.distance_horizontal += sim.simdt * abs(traf.gs)
         self.distance_vertical += sim.simdt * abs(traf.vs)
         
+        return
+        
+    # We want the plugin to run only when it is enabled via a stack command. 
+    # This is done by adding a command to the stack using the @stack.command decorator.
+    # The name of the function will then be the name of the command (or not, if it is specificed
+    # in the decorator arguments).
+    @stack.command()
+    def simplemetricsON(self):
+        self.enabled = True
+        
+        return
     
 
