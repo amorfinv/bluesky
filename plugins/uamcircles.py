4f4a160cc598f4f38c359f8c8124a0d27e6211ca
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2022-06-21 15:42:59 +0200

    uamcircles

--- plugins/uamcircles.py
+++ plugins/uamcircles.py
@@ -1,8 +1,10 @@
+from logging import warning
 import numpy as np
 from os import path
 
 import bluesky as bs
 from bluesky import settings, stack
+from bluesky.ui import palette
 from bluesky.ui.qtgl.gltraffic import Traffic
 from bluesky.ui.qtgl import glhelpers as glh
 
@@ -20,6 +22,13 @@ def init_plugin():
     # init_plugin() should always return a configuration dict.
     return config
 
+palette.set_default_colours(
+    caution=(255,255,0),
+    warning=(255,165,0),
+    collision=(255,0,0),
+)
+
+MAX_NAIRCRAFT = 10000
 
 # UAM traffic class
 class UAMTraffic(Traffic):
@@ -43,16 +52,22 @@ class UAMTraffic(Traffic):
         self.collision_circle = glh.Circle()
 
         # initialize values for radius
-        self.caution_radius = 0.0
-        self.warning_radius = 0.0
-        self.collision_radius = 0.0
-
-
+        self.caution_radius = 60.0
+        self.warning_radius = 40.0
+        self.collision_radius = 30.0
 
     def create(self):
         super().create()
 
-        # create the objects
+        # create buffers
+        self.caution_buffer.create(MAX_NAIRCRAFT * 4, glh.GLBuffer.UsagePattern.StreamDraw)
+        self.warning_buffer.create(MAX_NAIRCRAFT * 4, glh.GLBuffer.UsagePattern.StreamDraw)
+        self.collision_buffer.create(MAX_NAIRCRAFT * 4, glh.GLBuffer.UsagePattern.StreamDraw)
+
+        self.caution_color.create(MAX_NAIRCRAFT * 4, glh.GLBuffer.UsagePattern.StreamDraw)
+        self.warning_color.create(MAX_NAIRCRAFT * 4, glh.GLBuffer.UsagePattern.StreamDraw)
+        self.collision_color.create(MAX_NAIRCRAFT * 4, glh.GLBuffer.UsagePattern.StreamDraw)
+
         # link the circle buffers to the color and radius buffers
 
         self.caution_circle.create(radius=1.0)
@@ -60,11 +75,11 @@ class UAMTraffic(Traffic):
                                        color=self.caution_color, instance_divisor=1)
 
         self.warning_circle.create(radius=1.0)
-        self.warning_circle.set_attribs(lat=self.lat, lon=self.lon, scale=self.warning_circle,
+        self.warning_circle.set_attribs(lat=self.lat, lon=self.lon, scale=self.warning_buffer,
                                        color=self.warning_color, instance_divisor=1)
 
         self.collision_circle.create(radius=1.0)
-        self.collision_circle.set_attribs(lat=self.lat, lon=self.lon, scale=self.collision_circle,
+        self.collision_circle.set_attribs(lat=self.lat, lon=self.lon, scale=self.collision_buffer,
                                         color=self.collision_color, instance_divisor=1)
 
 
@@ -74,17 +89,49 @@ class UAMTraffic(Traffic):
         super().draw()
 
         actdata = bs.net.get_nodedata()
-
         #  circles only when they are bigger than the A/C symbols
-        if actdata.show_pz and actdata.zoom >= 0.15:
+        if actdata.zoom >= 0.15:
             self.shaderset.set_vertex_scale_type(
                 self.shaderset.VERTEX_IS_METERS)
 
             # draw the circles    
-            self.collision_circle.draw(n_instances=actdata.naircraft)
-            self.warning_circle.draw(n_instances=actdata.naircraft)
             self.caution_circle.draw(n_instances=actdata.naircraft)
+            self.warning_circle.draw(n_instances=actdata.naircraft)
+            self.collision_circle.draw(n_instances=actdata.naircraft)
 
+
+    def update_aircraft_data(self, data):
+        super().update_aircraft_data(data)
+
+        naircraft = len(data.lat)
+        if naircraft != 0:
+
+            # Here we assume that all circles are the same.
+            # if different then we need to make a sim plugin to communicate this info
+            caution_array = np.ones(naircraft, dtype=np.float32)*self.caution_radius
+            warning_array = np.ones(naircraft, dtype=np.float32)*self.warning_radius
+            collision_array = np.ones(naircraft, dtype=np.float32)*self.collision_radius
+
+            self.caution_buffer.update(np.array(caution_array, dtype=np.float32))
+            self.warning_buffer.update(np.array(warning_array, dtype=np.float32))
+            self.collision_buffer.update(np.array(collision_array, dtype=np.float32))
+            
+            
+            # set colors
+            caution_color = np.empty((min(naircraft, MAX_NAIRCRAFT), 4), dtype=np.uint8)
+            warning_color = np.empty((min(naircraft, MAX_NAIRCRAFT), 4), dtype=np.uint8)
+            collision_color = np.empty((min(naircraft, MAX_NAIRCRAFT), 4), dtype=np.uint8)
+
+            # TODO: no for loop needed probably
+            for i in range(naircraft):
+
+                caution_color[i, :] = tuple(palette.caution) + (255,)
+                warning_color[i, :] = tuple(palette.warning) + (255,)
+                collision_color[i, :] = tuple(palette.collision) + (255,)
+
+            self.caution_color.update(caution_color)
+            self.warning_color.update(warning_color)
+            self.collision_color.update(collision_color)
 
     @stack.command
     def uamcircles(self, caution_radius: float, warning_radius: float, collision_radius: float):
@@ -94,30 +141,3 @@ class UAMTraffic(Traffic):
         self.caution_radius = caution_radius
         self.warning_radius = warning_radius
         self.collision_radius = collision_radius
-
-
-    def update_aircraft_data(self, data):
-        super().update_aircraft_data(data)
-
-
-        # set the color of the circles
-        # TODO: make it dynamic
-        # set rgb red color tuple
-        rgb_red = (1.0, 0.0, 0.0)
-        rgb_orange = (1.0, 0.5, 0.0)
-        rgb_yellow = (1.0, 1.0, 0.0)
-
-        self.caution_color.update(rgb_red)
-        self.warning_color.update(rgb_orange)
-        self.collision_color.update(rgb_yellow)
-
-
-        # create a numpy array that is self.ntraf size long and stores the radius
-        caution_array = np.ones(self.ntraf, dtype=np.float32)*self.caution_radius
-        warning_array = np.ones(self.ntraf, dtype=np.float32)*self.warning_radius
-        collision_array = np.ones(self.ntraf, dtype=np.float32)*self.collision_radius
-        
-        # set the color of the circles
-        self.caution_buffer.update(np.array(caution_array, dtype=np.float32))
-        self.warning_buffer.update(np.array(warning_array, dtype=np.float32))
-        self.collision_buffer.update(np.array(collision_array, dtype=np.float32))
