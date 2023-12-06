1418cfbadf1888675bb851e0afd136846bda4a05
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2023-12-06 16:59:50 +0100

    update batch

--- scenario/FLOW/batchmaker.py
+++ scenario/FLOW/batchmaker.py
@@ -1,20 +1,22 @@
 import os
 
+scn_folder = 'observation'
+
 folders = ['conflict', 'intrusion', 'live']
 batches = {folder:[] for folder in folders}
 
-for folder in folders:
+for case_folder in folders:
 
     # get list of files
-    files_in_dir = os.listdir('observation/' + folder)
+    files_in_dir = os.listdir(scn_folder + '/' + case_folder)
     
     # step 1 go through one folder
     for file_name in files_in_dir:
 
-        lines = f'00:00:00>SCEN {folder}_{file_name[:-4]}_CRON\n' + \
-                f'00:00:00>PCALL FLOW/testing/{folder}/{file_name}\n'
+        lines = f'00:00:00>SCEN {case_folder}_{file_name[:-4]}_CROFF\n' + \
+                f'00:00:00>PCALL FLOW/{scn_folder}/{case_folder}/{file_name}\n'
         
-        batches[folder].append(lines)
+        batches[case_folder].append(lines)
 
 
 # create a file
