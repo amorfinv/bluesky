2054ff5109c6592efd336b872867b756e933999b
Andres Morfin Veytia <78442543+amorfinv@users.noreply.github.com>
2024-07-30 14:10:50 +0200

    add observation time scenarios

--- scenario/observationtime/scenariomaker.py
+++ scenario/observationtime/scenariomaker.py
@@ -4,7 +4,7 @@ scen_dir = 'conflict'
 
 experiment_cases = {
     'concepts' : {
-        'conflict'  : 'CONFLICTCLUSTERING'
+        'conflict'  : 'CONFLICTCLUSTERING',
     },
     'densities' : {
         '100',
@@ -14,16 +14,24 @@ experiment_cases = {
         '500',
     },
     'clusters' : {
-        '1000',
         '4000',
     },
-    'observationtime' : {
+    'replanlimit':[
+        '30',
+    ],
+    'replanratio':[
+        '0.5',
+    ],
+    'graphweights':[
+        '1-2-2',
+    ],
+    'observationtime':[
         '60',
         '300',
         '600',
         '900',
-        '1200',
-    },
+        '1200'
+    ],
     'seeds': [
         '748180',
         '825078',
@@ -38,6 +46,9 @@ exp_cases = list(
         experiment_cases['concepts'].keys(),
         experiment_cases['densities'],
         experiment_cases['clusters'],
+        experiment_cases['replanlimit'],
+        experiment_cases['replanratio'],
+        experiment_cases['graphweights'],
         experiment_cases['observationtime'],
         experiment_cases['seeds'],
     )
@@ -45,22 +56,36 @@ exp_cases = list(
 
 # LOG processing for indivdual experiments
 filenames = []
-for concept, density, cluster, observationtime, seed in exp_cases:
+for concept, density, cluster, replanlimit, replanratio, graph_weights, observationtime, seed in exp_cases:
 
     cluster_plugin = experiment_cases['concepts'][concept]
+    
+    low_weight, medium_weight, high_weight = graph_weights.split('-')
+    
+    # different flow control
+    flow_control = 'RANDOMFLOWCONTROL' if concept == 'random' else 'FLOWCONTROL'
 
     # add seeds
     lines_seed = [f'SEED {seed}']
 
+    flowseed = int(seed) + 1
+    lines_seed.append(f'FLOWSEED {flowseed}')
+
+    if concept == 'random':
+        clustseed = int(seed) + 2
+        lines_seed.append(f'CLUSTSEED {clustseed}')
+    
     # make a file for this case
     lines = [
-        f'SEED {seed}',
         'ENABLEFLOWCONTROL',
         'streetsenable',
         'STOPSIMT 7200',
+        f'SETGRAPHWEIGHTS {low_weight},{medium_weight},{high_weight}',
         'ASAS ON',
         'CDMETHOD M2CD',
         'RESO M2CR',
+        f'REPLANLIMIT {replanlimit}',
+        f'REPLANRATIO {replanratio}',
         'STARTLOGS',
         'STARTCDRLOGS',
         'STARTCLUSTERLOG',
@@ -68,11 +93,18 @@ for concept, density, cluster, observationtime, seed in exp_cases:
         f'trafficnumber {density}',
         f'SETCLUSTERDISTANCE {cluster}',
         f'SETOBSERVATIONTIME {observationtime}',
+
         'CASMACHTHR 0',
     ]
 
+    if concept in ['conflict', 'intrusion']:
+        lines.append('SETOBSERVATIONTIME 600')
+
     # add the fast forward
     lines.append('FF')
+
+    # comvine seeds lines
+    lines = lines_seed + lines
 
     # add the prefix to all lines
     lines = ['00:00:00.00>' + line for line in lines]
@@ -81,12 +113,11 @@ for concept, density, cluster, observationtime, seed in exp_cases:
     lines = '\n'.join(lines)
 
     # create a filename
-    filename = f'{concept}_traf{density}_clust{cluster}_observationtime{observationtime}_seed{seed}_CRON.scn'
+    filename = f'{concept}_traf{density}_clust{cluster}_replanlimit{replanlimit}_replanratio{replanratio}_graphweights{graph_weights}_observationtime{observationtime}_seed{seed}_CRON.scn'
 
     # write the file
     with open(f'{scen_dir}/{filename}', 'w') as file:
         file.write(lines + '\n') 
-
     
     filenames.append(filename)
 
